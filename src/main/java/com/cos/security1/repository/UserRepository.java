8be1b19364a471403d489dbd44daf520f8687ef9
qjvk2880 <qjvk2880@gmail.com>
2023-06-20 17:43:24 +0900

    Feat : findByUsername 메소드 추가

--- src/main/java/com/cos/security1/repository/UserRepository.java
+++ src/main/java/com/cos/security1/repository/UserRepository.java
@@ -2,6 +2,9 @@ package com.cos.security1.repository;
 
 import com.cos.security1.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    public User findByUsername(String username);
 }
